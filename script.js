387825b48743569b1b7ea5658865210f772d6425
Mika Benjamin <mbenjamin995@gmail.com>
2021-12-19 21:03:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let container = document.querySelector('.container');
+let button = document.querySelector('a');
+let displayHex = document.querySelector('p');
+
+const validCharacters = '0123456789ABCDEF';
+const hexDecimalLength = 6;
+
+function randomIntFromInterval(min, max){
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getNextHexColor(){
+	let color = '#';
+	for(let i = 0; i<6;i++){
+		let getRandomChar = validCharacters.charAt(randomIntFromInterval(0,15));
+		color += getRandomChar;
+	};
+	
+	displayHex.innerHTML = color;
+	container.style.backgroundColor = color;
+}
+
+getNextHexColor();
+
+button.addEventListener('click', function(){
+			getNextHexColor();	
+}, false);
